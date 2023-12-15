8364635f043f29a0b3fd91da20b54d4310d05816
zarif-al <zarif_al96@outlook.com>
2023-12-15 17:19:53 +0600

    feat: create a struct to handle general responses

---
+++ src/general_response.rs
@@ -0,0 +1,17 @@
+use serde::Serialize;
+
+/**
+This struct should be used in situations where the application
+is not able to return the regular response.
+
+For example:
+Lets say the app failed to create a user so it can't send the
+new user as a response. In that case it should return a general response
+containing the error message.
+*/
+#[derive(Serialize)]
+pub struct GeneralResponse {
+    pub code: u8,
+    pub message: Option<String>,
+    pub error: Option<String>,
+}
