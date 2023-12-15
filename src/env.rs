238afad4631325eeb705172ee428d6f96b24f036
zarif-al <zarif_al96@outlook.com>
2023-12-15 18:21:01 +0600

    feat: create module to read and return values of the `.env` file

---
+++ src/env.rs
@@ -0,0 +1,22 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
+pub struct Env {
+    pub app_name: String,
+    pub port: u16,
+}
+
+pub fn get_env() -> Env {
+    dotenv().ok();
+
+    let app_name = env::var("APP_NAME").unwrap_or("Rust GraphQL Service".to_string());
+
+    let port = env::var("PORT")
+        .unwrap_or("4000".to_string())
+        .parse()
+        .expect("PORT is expected to be parsable as u32");
+
+    Env { app_name, port }
+}
