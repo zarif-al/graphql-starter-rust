d2ed51d7efb48d260c560e15b4f344c1da07a7a4
zarif-al <zarif_al96@outlook.com>
2023-12-15 23:54:50 +0600

    refactor: use struct for `create_user` input

--- src/graphql/mutation.rs
+++ src/graphql/mutation.rs
@@ -2,16 +2,16 @@
 ///
 /// Ideally you should contain the core logic of a mutation in
 /// a separate module and call those functions/methods from here.
-use async_graphql::{Json, Object};
+use async_graphql::{Object, Result};
 
-use crate::user::create::create_user;
+use crate::user::create::{create_user, CreateUser};
 use crate::user::definition::User;
 
 pub struct MutationRoot;
 
 #[Object]
 impl MutationRoot {
-    pub async fn create_user(&self, username: String) -> Json<User> {
-        create_user(username)
+    pub async fn create_user(&self, input: CreateUser) -> Result<User> {
+        create_user(input)
     }
 }
