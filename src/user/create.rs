a9c64d480fddb2a03ac6da22df713705cc3fb1c2
zarif-al <zarif_al96@outlook.com>
2023-12-15 23:55:25 +0600

    refactor: create a struct for the `create_user` mutation input

--- src/user/create.rs
+++ src/user/create.rs
@@ -1,13 +1,21 @@
-use async_graphql::Json;
+use crate::user::definition::User;
+use async_graphql::{Error, InputObject, Result};
 use tracing::info;
 
-use crate::user::definition::User;
+#[derive(InputObject)]
+pub struct CreateUser {
+    username: String,
+}
 
-pub fn create_user(username: String) -> Json<User> {
-    info!("Creating user with argument: {}", username);
+pub fn create_user(input: CreateUser) -> Result<User> {
+    if input.username.is_empty() {
+        return Err(Error::new("Invalid username value"));
+    }
 
-    Json(User {
+    info!("Creating user with argument: {}", input.username);
+
+    Ok(User {
         id: 1337,
-        username: username,
+        username: input.username,
     })
 }
