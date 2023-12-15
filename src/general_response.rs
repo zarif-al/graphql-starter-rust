b9edf3f7cee725d07b7aed8c7be80d8092d7c3b3
zarif-al <zarif_al96@outlook.com>
2023-12-15 17:21:06 +0600

    docs: update doc style

--- src/general_response.rs
+++ src/general_response.rs
@@ -1,14 +1,11 @@
 use serde::Serialize;
 
-/**
-This struct should be used in situations where the application
-is not able to return the regular response.
-
-For example:
-Lets say the app failed to create a user so it can't send the
-new user as a response. In that case it should return a general response
-containing the error message.
-*/
+///  This struct should be used in situations where the application
+/// is not able to return the regular response.
+/// For example:
+/// Lets say the app failed to create a user so it can't send the
+/// new user as a response. In that case it should return a general response
+/// containing the error message.
 #[derive(Serialize)]
 pub struct GeneralResponse {
     pub code: u8,
