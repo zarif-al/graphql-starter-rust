83a655ef844daac95a2764a5ba7ce32af4932c6b
zarif-al <zarif_al96@outlook.com>
2023-12-23 12:17:44 +0600

    refactor: insert user in database

--- src/graphql/mutation.rs
+++ src/graphql/mutation.rs
@@ -4,14 +4,18 @@
 /// a separate module and call those functions/methods from here.
 use async_graphql::{Object, Result};
 
-use crate::user::create::{create_user, CreateUser};
-use crate::user::definition::User;
+use crate::repositories::user::{
+    create::{create_user, CreateUser},
+    definition::GraphQLUser,
+};
 
 pub struct MutationRoot;
 
 #[Object]
 impl MutationRoot {
-    pub async fn create_user(&self, input: CreateUser) -> Result<User> {
-        create_user(input)
+    pub async fn create_user(&self, input: CreateUser) -> Result<GraphQLUser> {
+        let new_user = create_user(input).await?;
+        let response: GraphQLUser = new_user.into();
+        Ok(response)
     }
 }
