5a2c582d6d1169f7ddbf7feb726ce5c34e66b735
zarif-al <zarif_al96@outlook.com>
2023-12-23 23:18:28 +0600

    feat: update test case

--- src/repositories/user/create.rs
+++ src/repositories/user/create.rs
@@ -2,15 +2,20 @@ use async_graphql::{Error, InputObject, Result};
 use sea_orm::{ActiveModelTrait, ActiveValue, DatabaseConnection};
 use tracing::info;
 
-use crate::entities::user::{self, Model};
+use crate::entities::user::{self};
+
+use super::GraphQLUser;
 
 #[derive(InputObject)]
 pub struct CreateUser {
     first_name: String,
     last_name: String,
+    email: String,
 }
 
-pub async fn create_user(db: &DatabaseConnection, input: CreateUser) -> Result<Model> {
+pub async fn create_user(db: &DatabaseConnection, input: CreateUser) -> Result<GraphQLUser> {
+    println!("Called");
+
     if input.first_name.is_empty() {
         return Err(Error::new("Invalid username value"));
     }
@@ -22,7 +27,7 @@ pub async fn create_user(db: &DatabaseConnection, input: CreateUser) -> Result<M
     let new_user = user::ActiveModel {
         first_name: ActiveValue::Set(input.first_name),
         last_name: ActiveValue::Set(input.last_name),
-        email: ActiveValue::Set("test@email.com".to_string()),
+        email: ActiveValue::Set(input.email),
         ..Default::default()
     };
 
@@ -33,26 +38,61 @@ pub async fn create_user(db: &DatabaseConnection, input: CreateUser) -> Result<M
         result.first_name, result.last_name
     );
 
-    Ok(result)
+    Ok(result.into())
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::user::create::create_user;
+#[cfg(test)]
+mod tests {
+    use super::create_user;
+    use super::CreateUser;
+    use crate::entities::user;
+    use crate::repositories::user::GraphQLUser;
+    use chrono::Utc;
+    use sea_orm::{DatabaseBackend, MockDatabase};
 
-//     use super::CreateUser;
+    #[async_std::test]
+    async fn test_create_user() {
+        let creation_time = Utc::now();
+        let updated_time = Utc::now();
+        let first_name = "Test".to_string();
+        let last_name = "User".to_string();
+        let email = "test@email.com".to_string();
 
-//     #[test]
-//     fn test_create_user() {
-//         let username = String::from("Test User");
+        let expected_result: GraphQLUser = GraphQLUser {
+            created_at: creation_time.timestamp(),
+            updated_at: updated_time.timestamp(),
+            first_name: first_name.clone(),
+            last_name: last_name.clone(),
+            email: email.clone(),
+        };
 
-//         let input = CreateUser {
-//             username: username.clone(),
-//         };
+        // Create MockDatabase with mock query results
+        let db = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_results([vec![user::Model {
+                first_name: first_name.clone(),
+                last_name: last_name.clone(),
+                email: email.clone(),
+                created_at: creation_time.into(),
+                updated_at: updated_time.into(),
+            }]])
+            .into_connection();
 
-//         let result = create_user(input).expect("User should be successfully created.");
+        let result = create_user(
+            &db,
+            CreateUser {
+                first_name: first_name.clone(),
+                last_name: last_name.clone(),
+                email: email.clone(),
+            },
+        )
+        .await
+        .unwrap();
 
-//         assert_eq!(result.id, 1337);
-//         assert_eq!(result.username, username);
-//     }
-// }
+        // Check result against expected result
+        assert_eq!(result.created_at, expected_result.created_at);
+        assert_eq!(result.updated_at, expected_result.updated_at);
+        assert_eq!(result.first_name, expected_result.first_name);
+        assert_eq!(result.last_name, expected_result.last_name);
+        assert_eq!(result.email, expected_result.email);
+    }
+}
