82982ade03000d6386f5a02a04f9a4dc8b75cf8f
zarif-al <zarif_al96@outlook.com>
2023-12-23 20:54:59 +0600

    refactor: remove database connection check

--- src/graphql/query.rs
+++ src/graphql/query.rs
@@ -4,26 +4,17 @@
 /// a separate module and call those functions/methods from here.
 use async_graphql::{Json, Object};
 
-use crate::misc::{get_db_connection, responses::GeneralResponse};
+use crate::misc::responses::GeneralResponse;
 
 pub struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
     pub async fn health_check(&self) -> Json<GeneralResponse> {
-        let db = get_db_connection().await;
-
-        match db {
-            Ok(_) => Json(GeneralResponse {
-                code: 200,
-                error: None,
-                message: Some("Database Connection Established!".to_string()),
-            }),
-            Err(e) => Json(GeneralResponse {
-                code: 500,
-                error: Some(e.to_string()),
-                message: None,
-            }),
-        }
+        Json(GeneralResponse {
+            code: 200,
+            error: None,
+            message: Some("Application is up and running!".to_string()),
+        })
     }
 }
