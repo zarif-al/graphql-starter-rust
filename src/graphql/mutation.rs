d329252e4dad2d3006693cf3c0157ae9fd39ff1a
zarif-al <zarif_al96@outlook.com>
2023-12-23 20:57:21 +0600

    refactor: use db connection from context, and pass it to the user create function

--- src/graphql/mutation.rs
+++ src/graphql/mutation.rs
@@ -2,7 +2,8 @@
 ///
 /// Ideally you should contain the core logic of a mutation in
 /// a separate module and call those functions/methods from here.
-use async_graphql::{Object, Result};
+use async_graphql::{Context, Object, Result};
+use sea_orm::DatabaseConnection;
 
 use crate::repositories::user::{
     create::{create_user, CreateUser},
@@ -13,9 +14,20 @@ pub struct MutationRoot;
 
 #[Object]
 impl MutationRoot {
-    pub async fn create_user(&self, input: CreateUser) -> Result<GraphQLUser> {
-        let new_user = create_user(input).await?;
-        let response: GraphQLUser = new_user.into();
-        Ok(response)
+    pub async fn create_user<'ctx>(
+        &self,
+        ctx: &Context<'ctx>,
+        input: CreateUser,
+    ) -> Result<GraphQLUser> {
+        let db_connection = ctx.data::<DatabaseConnection>();
+
+        match db_connection {
+            Ok(db) => {
+                let new_user = create_user(&db, input).await?;
+                let response: GraphQLUser = new_user.into();
+                Ok(response)
+            }
+            Err(err) => Err(err),
+        }
     }
 }
