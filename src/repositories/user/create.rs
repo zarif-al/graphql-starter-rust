8b8557edd7927cc4c369391b86dee280b4b66072
zarif-al <zarif_al96@outlook.com>
2023-12-25 15:28:53 +0600

    refactor: comment out the test code

--- src/repositories/user/create.rs
+++ src/repositories/user/create.rs
@@ -41,58 +41,61 @@ pub async fn create_user(db: &DatabaseConnection, input: CreateUser) -> Result<G
     Ok(result.into())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::create_user;
-    use super::CreateUser;
-    use crate::entities::user;
-    use crate::repositories::user::GraphQLUser;
-    use chrono::Utc;
-    use sea_orm::{DatabaseBackend, MockDatabase};
+// So far I have not found a reasonable testing strategy. I will resume this later.
 
-    #[async_std::test]
-    async fn test_create_user() {
-        let creation_time = Utc::now();
-        let updated_time = Utc::now();
-        let first_name = "Test".to_string();
-        let last_name = "User".to_string();
-        let email = "test@email.com".to_string();
+// #[cfg(test)]
+// mod tests {
+//     use super::create_user;
+//     use super::CreateUser;
+//     use crate::entities::user;
+//     use crate::repositories::user::GraphQLUser;
+//     use chrono::Utc;
+//     use sea_orm::DbErr;
+//     use sea_orm::{DatabaseBackend, MockDatabase};
 
-        let expected_result: GraphQLUser = GraphQLUser {
-            created_at: creation_time.timestamp(),
-            updated_at: updated_time.timestamp(),
-            first_name: first_name.clone(),
-            last_name: last_name.clone(),
-            email: email.clone(),
-        };
+//     #[async_std::test]
+//     async fn test_create_user_successfull() {
+//         let creation_time = Utc::now();
+//         let updated_time = Utc::now();
+//         let first_name = "Test".to_string();
+//         let last_name = "User".to_string();
+//         let email = "test@email.com".to_string();
 
-        // Create MockDatabase with mock query results
-        let db = MockDatabase::new(DatabaseBackend::Postgres)
-            .append_query_results([vec![user::Model {
-                first_name: first_name.clone(),
-                last_name: last_name.clone(),
-                email: email.clone(),
-                created_at: creation_time.into(),
-                updated_at: updated_time.into(),
-            }]])
-            .into_connection();
+//         let expected_result: GraphQLUser = GraphQLUser {
+//             created_at: creation_time.timestamp(),
+//             updated_at: updated_time.timestamp(),
+//             first_name: first_name.clone(),
+//             last_name: last_name.clone(),
+//             email: email.clone(),
+//         };
 
-        let result = create_user(
-            &db,
-            CreateUser {
-                first_name: first_name.clone(),
-                last_name: last_name.clone(),
-                email: email.clone(),
-            },
-        )
-        .await
-        .unwrap();
+//         // Create MockDatabase with mock query results
+//         let db = MockDatabase::new(DatabaseBackend::Postgres)
+//             .append_query_results([vec![user::Model {
+//                 first_name: first_name.clone(),
+//                 last_name: last_name.clone(),
+//                 email: email.clone(),
+//                 created_at: creation_time.into(),
+//                 updated_at: updated_time.into(),
+//             }]])
+//             .into_connection();
 
-        // Check result against expected result
-        assert_eq!(result.created_at, expected_result.created_at);
-        assert_eq!(result.updated_at, expected_result.updated_at);
-        assert_eq!(result.first_name, expected_result.first_name);
-        assert_eq!(result.last_name, expected_result.last_name);
-        assert_eq!(result.email, expected_result.email);
-    }
-}
+//         let result = create_user(
+//             &db,
+//             CreateUser {
+//                 first_name: first_name.clone(),
+//                 last_name: last_name.clone(),
+//                 email: email.clone(),
+//             },
+//         )
+//         .await
+//         .unwrap();
+
+//         // Check result against expected result
+//         assert_eq!(result.created_at, expected_result.created_at);
+//         assert_eq!(result.updated_at, expected_result.updated_at);
+//         assert_eq!(result.first_name, expected_result.first_name);
+//         assert_eq!(result.last_name, expected_result.last_name);
+//         assert_eq!(result.email, expected_result.email);
+//     }
+// }
