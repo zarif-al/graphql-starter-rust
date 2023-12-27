94ed1ae567e2ae304cd311e7c683148b83b15933
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-12-27 13:30:27 +0600

    feat: add find user query

--- src/graphql/query.rs
+++ src/graphql/query.rs
@@ -2,9 +2,16 @@
 ///
 /// Ideally you should contain the core logic of a query in
 /// a separate module and call those functions/methods from here.
-use async_graphql::{Json, Object};
+use async_graphql::{Context, Json, Object, Result};
+use sea_orm::DatabaseConnection;
 
-use crate::misc::responses::GeneralResponse;
+use crate::{
+    misc::responses::GeneralResponse,
+    repositories::user::{
+        find::{find_one_user, FindOne},
+        GraphQLUser,
+    },
+};
 
 pub struct QueryRoot;
 
@@ -16,5 +23,25 @@ impl QueryRoot {
             error: None,
             message: Some("Application is up and running!".to_string()),
         })
+    }
+
+    pub async fn find_user<'ctx>(
+        &self,
+        ctx: &Context<'ctx>,
+        input: FindOne,
+    ) -> Result<Option<GraphQLUser>> {
+        let db_connection = ctx.data::<DatabaseConnection>();
+
+        match db_connection {
+            Ok(db) => {
+                let user = find_one_user(&db, input).await?;
+
+                match user {
+                    Some(user) => Ok(Some(user)),
+                    None => Ok(None),
+                }
+            }
+            Err(err) => Err(err),
+        }
     }
 }
