5a65f5cb68719cc80fd8c47301de5069deff5a30
zarif-al <zarif_al96@outlook.com>
2023-12-29 09:49:54 +0600

    format: remove space

--- src/misc/responses.rs
+++ src/misc/responses.rs
@@ -1,7 +1,7 @@
 use async_graphql::SimpleObject;
 use serde::Serialize;
 
-///  This struct should be used in situations where the application
+/// This struct should be used in situations where the application
 /// is not able to return the regular response.
 /// For example:
 /// Lets say the app failed to create a user so it can't send the
