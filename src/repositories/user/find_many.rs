5d32f9c2c76313f41db084cb3afea68d4352d715
zarif-al <zarif_al96@outlook.com>
2023-12-30 17:47:47 +0600

    refactor: split find methods into separate files

---
+++ src/repositories/user/find_many.rs
@@ -0,0 +1,135 @@
+use async_graphql::{InputObject, Result};
+use sea_orm::{DatabaseConnection, EntityTrait, PaginatorTrait, QueryOrder};
+
+use crate::entities::user;
+
+use super::GraphQLUser;
+
+#[derive(InputObject)]
+pub struct FindUsersInput {
+    first_name: Option<String>,
+    last_name: Option<String>,
+    email: Option<String>,
+    limit: u64,
+    page: u64,
+}
+
+pub async fn find_users(
+    db: &DatabaseConnection,
+    input: FindUsersInput,
+) -> Result<Vec<GraphQLUser>> {
+    let page_size;
+
+    if input.limit == 0 {
+        page_size = 1
+    } else {
+        page_size = input.limit
+    }
+
+    // Offset based pagination
+    let paginate = user::Entity::find()
+        .order_by_asc(user::Column::FirstName)
+        .paginate(db, page_size);
+
+    let user_pages = paginate;
+
+    let users = user_pages.fetch_page(input.page).await?;
+
+    Ok(users.into_iter().map(|user| user.into()).collect())
+
+    // Cursor based pagination
+    // let mut cursor = user::Entity::find().cursor_by(user::Column::FirstName);
+    // if let Some(after) = input.after {
+    //     cursor.after(after);
+    // }
+    // let results = cursor.first(input.limit).all(db).await?;
+    // Ok(results.into_iter().map(|user| user.into()).collect())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{find_users, FindUsersInput};
+    use crate::entities::user::Model;
+    use crate::repositories::user::_mock::get_mock_create_user_input;
+    use crate::repositories::user::_mock::get_mock_user_model;
+    use sea_orm::DbErr;
+    use sea_orm::Transaction;
+    use sea_orm::{DatabaseBackend, MockDatabase};
+
+    #[async_std::test]
+    /// Test a successfull user find operation that returns a vector of users
+    async fn find_users_success() {
+        // Instantiate find users input argument
+        let input_argument: FindUsersInput = FindUsersInput {
+            first_name: Some("Test".to_string()),
+            last_name: Some("User".to_string()),
+            email: Some("test@email.com".to_string()),
+            limit: 0,
+            page: 0,
+        };
+
+        // Create mock db users
+        let mut mock_db_users: Vec<Model> = vec![];
+        while mock_db_users.len() < 10 {
+            let create_user_input = get_mock_create_user_input();
+            let mock_db_user: Model = get_mock_user_model(create_user_input.clone());
+
+            mock_db_users.push(mock_db_user);
+        }
+
+        // Create MockDatabase with mock query results
+        let db = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_results([mock_db_users.clone()])
+            .into_connection();
+
+        // Run `find_users` function
+        let result = find_users(&db, input_argument).await.unwrap();
+
+        // Check result against expected result
+        assert_eq!(result.len(), mock_db_users.len());
+
+        // Check the transaction log to make sure the correct SQL operation is being run
+        assert_eq!(
+            db.into_transaction_log(),
+            [Transaction::from_sql_and_values(
+                DatabaseBackend::Postgres,
+                r#"SELECT "user"."created_at", "user"."updated_at", "user"."first_name", "user"."last_name", "user"."email" FROM "user" ORDER BY "user"."first_name" ASC LIMIT $1 OFFSET $2"#,
+                [1u64.into(), 0u64.into()]
+            )]
+        )
+    }
+
+    #[async_std::test]
+    /// Test a users find operation that fails
+    async fn find_users_error() {
+        // Instantiate find users input argument
+        let input_argument: FindUsersInput = FindUsersInput {
+            first_name: Some("Test".to_string()),
+            last_name: Some("User".to_string()),
+            email: Some("test@email.com".to_string()),
+            limit: 0,
+            page: 0,
+        };
+
+        // Create MockDatabase with mock query error
+        let db = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_errors(vec![DbErr::Custom("Unknown Error".to_string())])
+            .into_connection();
+
+        // Run `find_users` function
+        let result = find_users(&db, input_argument).await;
+
+        // Check that the result is an error.
+        assert!(result.is_err());
+
+        // Check the transaction log to make sure the correct SQL operation is being run
+        assert_eq!(
+            db.into_transaction_log(),
+            [Transaction::from_sql_and_values(
+                DatabaseBackend::Postgres,
+                r#"SELECT "user"."created_at", "user"."updated_at", "user"."first_name", "user"."last_name", "user"."email" FROM "user" ORDER BY "user"."first_name" ASC LIMIT $1 OFFSET $2"#,
+                [1u64.into(), 0u64.into()]
+            )]
+        )
+    }
+}
---
+++ src/repositories/user/find_one.rs
@@ -0,0 +1,151 @@
+use async_graphql::{InputObject, Result};
+use sea_orm::{DatabaseConnection, EntityTrait};
+
+use crate::entities::user::Entity as User;
+
+use super::GraphQLUser;
+
+#[derive(InputObject)]
+pub struct FindUserInput {
+    email: String,
+}
+
+pub async fn find_user(
+    db: &DatabaseConnection,
+    input: FindUserInput,
+) -> Result<Option<GraphQLUser>> {
+    let result = User::find_by_id(input.email).one(db).await?;
+
+    match result {
+        Some(user) => Ok(Some(user.into())),
+        None => Ok(None),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{find_user, FindUserInput};
+    use crate::entities::user::Model;
+    use crate::repositories::user::GraphQLUser;
+    use crate::repositories::user::_mock::get_mock_create_user_input;
+    use crate::repositories::user::_mock::get_mock_graphql_user;
+    use crate::repositories::user::_mock::get_mock_user_model;
+    use sea_orm::DbErr;
+    use sea_orm::Transaction;
+    use sea_orm::{DatabaseBackend, MockDatabase};
+
+    #[async_std::test]
+    /// Test a successfull user find operation that returns some user
+    async fn find_user_some() {
+        // Create mock db user
+        let create_user_input = get_mock_create_user_input();
+        let mock_db_user: Model = get_mock_user_model(create_user_input.clone());
+
+        // Instantiate expected results
+        let expected_result: GraphQLUser = get_mock_graphql_user(mock_db_user.clone());
+
+        // Create MockDatabase with mock query results
+        let db = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_results([vec![mock_db_user.clone()]])
+            .into_connection();
+
+        // Run `find_user` function
+        let result = find_user(
+            &db,
+            FindUserInput {
+                email: mock_db_user.clone().email,
+            },
+        )
+        .await
+        .unwrap()
+        .unwrap();
+
+        // Check result against expected result
+        assert_eq!(result.created_at, expected_result.created_at);
+        assert_eq!(result.updated_at, expected_result.updated_at);
+        assert_eq!(result.first_name, expected_result.first_name);
+        assert_eq!(result.last_name, expected_result.last_name);
+        assert_eq!(result.email, expected_result.email);
+
+        // Check the transaction log to make sure the correct SQL operation is being run
+        assert_eq!(
+            db.into_transaction_log(),
+            [Transaction::from_sql_and_values(
+                DatabaseBackend::Postgres,
+                r#"SELECT "user"."created_at", "user"."updated_at", "user"."first_name", "user"."last_name", "user"."email" FROM "user" WHERE "user"."email" = $1 LIMIT $2"#,
+                [mock_db_user.email.into(), 1u64.into()]
+            )]
+        )
+    }
+
+    #[async_std::test]
+    /// Test a user find operation that returns NONE
+    async fn find_user_none() {
+        // Create mock db user
+        let create_user_input = get_mock_create_user_input();
+        let mock_db_user: Model = get_mock_user_model(create_user_input.clone());
+
+        // Create MockDatabase with mock query results
+        let db = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_results([Vec::<Model>::new()])
+            .into_connection();
+
+        // Run `find_user` function
+        let result = find_user(
+            &db,
+            FindUserInput {
+                email: mock_db_user.clone().email,
+            },
+        )
+        .await
+        .unwrap();
+
+        // Check that the result is a NONE.
+        assert!(result.is_none());
+
+        // Check the transaction log to make sure the correct SQL operation is being run
+        assert_eq!(
+            db.into_transaction_log(),
+            [Transaction::from_sql_and_values(
+                DatabaseBackend::Postgres,
+                r#"SELECT "user"."created_at", "user"."updated_at", "user"."first_name", "user"."last_name", "user"."email" FROM "user" WHERE "user"."email" = $1 LIMIT $2"#,
+                [mock_db_user.email.into(), 1u64.into()]
+            )]
+        )
+    }
+
+    #[async_std::test]
+    /// Test a user find operation that fails
+    async fn find_user_error() {
+        // Create mock db user
+        let create_user_input = get_mock_create_user_input();
+        let mock_db_user: Model = get_mock_user_model(create_user_input.clone());
+
+        // Create MockDatabase with mock query error
+        let db = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_errors(vec![DbErr::Custom("Unknown Error".to_string())])
+            .into_connection();
+
+        // Run `find_user` function
+        let result = find_user(
+            &db,
+            FindUserInput {
+                email: mock_db_user.clone().email,
+            },
+        )
+        .await;
+
+        // Check that the result is an error.
+        assert!(result.is_err());
+
+        // Check the transaction log to make sure the correct SQL operation is being run
+        assert_eq!(
+            db.into_transaction_log(),
+            [Transaction::from_sql_and_values(
+                DatabaseBackend::Postgres,
+                r#"SELECT "user"."created_at", "user"."updated_at", "user"."first_name", "user"."last_name", "user"."email" FROM "user" WHERE "user"."email" = $1 LIMIT $2"#,
+                [mock_db_user.email.into(), 1u64.into()]
+            )]
+        )
+    }
+}
