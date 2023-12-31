36372633bf08186c32dc1277f6f93c13ed839acc
zarif-al <zarif_al96@outlook.com>
2023-12-31 08:13:27 +0600

    feat: create users seed function

--- seeder/src/main.rs
+++ seeder/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+
+use crate::user::{generate_mock_users, seed_users};
+
+mod user;
+
+#[tokio::main]
+async fn main() {
+    // Load env from .env file
+    dotenv().ok();
+
+    // Setup logger
+    // Update the `with_env_filter()` to get more or less logs
+    tracing_subscriber::fmt().with_env_filter("seeder").init();
+
+    // Generate mock_users
+    let mock_users = generate_mock_users(10);
+
+    // Seed users
+    let seed_users_results = seed_users(mock_users).await;
+    match seed_users_results {
+        Ok(()) => {
+            tracing::info!("Users seed complete!");
+        }
+        Err(e) => {
+            tracing::error!("Failed to seed users. Error: {}", e.to_string());
+        }
+    }
 }
