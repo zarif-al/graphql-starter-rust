503a5f29178204ef3f685d431ba9b285b4f103d2
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-12-31 10:33:44 +0600

    feat: add reset_db functionality

--- seeder/src/main.rs
+++ seeder/src/main.rs
@@ -1,7 +1,8 @@
-use dotenv::dotenv;
-
 use crate::user::{generate_mock_users, seed_users};
+use dotenv::dotenv;
+use reset_db::reset_database;
 
+mod reset_db;
 mod user;
 
 #[tokio::main]
@@ -12,6 +13,17 @@ async fn main() {
     // Setup logger
     // Update the `with_env_filter()` to get more or less logs
     tracing_subscriber::fmt().with_env_filter("seeder").init();
+
+    // Before seeding the database we should reset the database
+    // Cancel the seed operation if this step fails
+    let reset_db = reset_database().await;
+    match reset_db {
+        Err(e) => {
+            tracing::error!("Failed to reset database. Error: {}", e.to_string());
+            panic!()
+        }
+        _ => {}
+    }
 
     // Generate mock_users
     let mock_users = generate_mock_users(10);
