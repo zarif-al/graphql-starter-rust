7c6c214a19535cf4c450a21f44568ddc26f4b740
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-12-31 10:33:29 +0600

    feat: create module to reset database

---
+++ seeder/src/reset_db.rs
@@ -0,0 +1,24 @@
+use migration::Migrator;
+use sea_orm_migration::prelude::*;
+use server::misc::get_db_connection;
+
+pub async fn reset_database() -> Result<(), DbErr> {
+    let db = get_db_connection().await;
+
+    match db {
+        Ok(db) => {
+            tracing::info!("Resetting database...");
+            let migration_results = Migrator::refresh(&db).await;
+
+            match migration_results {
+                Ok(()) => {
+                    tracing::info!("Database reset successfully!");
+
+                    Ok(())
+                }
+                Err(e) => Err(e),
+            }
+        }
+        Err(e) => Err(e),
+    }
+}
