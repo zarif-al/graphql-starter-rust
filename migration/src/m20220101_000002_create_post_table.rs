a9b3aa52b158989275d4cb2b664320c794d1ee11
zarif-al <zarif_al96@outlook.com>
2023-12-31 22:40:25 +0600

    feat: add `post` migration

---
+++ migration/src/m20220101_000002_create_post_table.rs
@@ -0,0 +1,74 @@
+use sea_orm_migration::prelude::*;
+
+use crate::m20220101_000001_create_user_table::User;
+
+pub struct Migration;
+
+impl MigrationName for Migration {
+    fn name(&self) -> &str {
+        "m20220101_000002_create_post_table"
+    }
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    // Define how to apply this migration: Create the User table.
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Post::Table)
+                    .col(
+                        ColumnDef::new(Post::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(Post::CreatedAt)
+                            .timestamp_with_time_zone()
+                            .default(Expr::current_timestamp())
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(Post::UpdatedAt)
+                            .timestamp_with_time_zone()
+                            .default(Expr::current_timestamp())
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(Post::UserId).integer().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-post-user_id")
+                            .from(Post::Table, Post::UserId)
+                            .to(User::Table, User::Id),
+                    )
+                    .col(ColumnDef::new(Post::Content).string().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    // Define how to rollback this migration: Drop the Bakery table.
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(Iden)]
+pub enum Post {
+    Table,
+    #[iden = "id"]
+    Id,
+    #[iden = "created_at"]
+    CreatedAt,
+    #[iden = "updated_at"]
+    UpdatedAt,
+    #[iden = "user_id"]
+    UserId,
+    #[iden = "content"]
+    Content,
+}
