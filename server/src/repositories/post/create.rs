be85a39dbfbd48ab70944e3536a0584529b11b4f
zarif-al <zarif_al96@outlook.com>
2023-12-31 22:43:33 +0600

    feat: define `create post` function

---
+++ server/src/repositories/post/create.rs
@@ -0,0 +1,52 @@
+use async_graphql::{Error, InputObject, Result};
+use sea_orm::{ActiveModelTrait, DatabaseConnection, EntityTrait, Set};
+use server::entities::{post, user};
+
+use super::GraphQLPost;
+
+#[derive(InputObject)]
+pub struct CreatePost {
+    user_id: i32,
+    content: String,
+}
+
+pub async fn create_post(db: &DatabaseConnection, input: CreatePost) -> Result<GraphQLPost> {
+    // Verify content is not empty string.
+    if input.content.is_empty() {
+        return Err(Error::new("Invalid post content"));
+    }
+
+    // Search for user with provided user_id.
+    let user = user::Entity::find_by_id(input.user_id).one(db).await;
+
+    match user {
+        Ok(user) => {
+            // If user exists create post otherwise throw error.
+            if user.is_some() {
+                let new_post = post::ActiveModel {
+                    content: Set(input.content),
+                    user_id: Set(input.user_id),
+                    ..Default::default()
+                };
+
+                let result = new_post.insert(db).await;
+
+                match result {
+                    Ok(post_model) => Ok(post_model.into()),
+                    Err(e) => {
+                        tracing::error!("Source: Create Post. Message: {}", e.to_string());
+                        // Error: Failed to create post
+                        Err(Error::new("P101"))
+                    }
+                }
+            } else {
+                // Error: User does not exist
+                return Err(Error::new("P100"));
+            }
+        }
+        Err(e) => {
+            tracing::error!("Source: Create Post. Message: {}", e.to_string());
+            Err(Error::new("500"))
+        }
+    }
+}
