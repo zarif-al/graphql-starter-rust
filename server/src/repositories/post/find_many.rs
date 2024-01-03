e12c74a0b17fcca3463af22740d147a1f176e97a
zarif-al <zarif_al96@outlook.com>
2024-01-03 22:15:21 +0600

    feat: implement find posts by user

--- server/src/repositories/post/find_many.rs
+++ server/src/repositories/post/find_many.rs
@@ -1,47 +1,43 @@
-use crate::entities::post;
 use async_graphql::{Error, InputObject, Result};
 use sea_orm::{
     ColumnTrait, DatabaseConnection, EntityTrait, PaginatorTrait, QueryFilter, QueryOrder,
 };
 
+use crate::entities::post;
+
 use super::GraphQLPost;
 
 #[derive(InputObject)]
-pub struct FindPostsByUserIdInput {
+pub struct FindUserPostsInput {
     user_id: i32,
     limit: u64,
     page: u64,
 }
 
-pub async fn find_posts_by_user_id(
+pub async fn find_user_posts(
     db: &DatabaseConnection,
-    input: FindPostsByUserIdInput,
+    input: FindUserPostsInput,
 ) -> Result<Vec<GraphQLPost>> {
-    // TODO: Look into find_also_related method to eager load users
-
-    // Page size cannot be 0
     let page_size;
-    if input.limit < 1 {
+
+    if input.limit == 0 {
         page_size = 1
     } else {
         page_size = input.limit
     }
 
-    // Create post pages pagination by user id
+    // Offset based pagination
     let post_pages = post::Entity::find()
-        .order_by_asc(post::Column::Id)
+        .order_by_asc(post::Column::UpdatedAt)
         .filter(post::Column::UserId.eq(input.user_id))
         .paginate(db, page_size);
 
-    let post_search_results = post_pages.fetch_page(input.page).await;
+    let results = post_pages.fetch_page(input.page).await;
 
-    match post_search_results {
-        Ok(posts) => Ok(posts
-            .into_iter()
-            .map(|post: post::Model| post.into())
-            .collect()),
+    match results {
+        Ok(posts) => Ok(posts.into_iter().map(|post| post.into()).collect()),
         Err(e) => {
-            tracing::error!("Source: Find posts by user_id. Message: {}", e.to_string());
+            tracing::error!("Source: Find user posts. Message: {}", e.to_string());
             Err(Error::new("500"))
         }
     }
