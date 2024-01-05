1cc5be9381aeaf81d2581c03c4134e0caa7d3b39
Abdullah Al Zarif <zarif_al96@outlook.com>
2024-01-05 20:13:43 +0600

    feat: create seed function for post

--- seeder/src/post.rs
+++ seeder/src/post.rs
@@ -1,16 +1,47 @@
-pub fn generate_seed_posts(num: usize, user_ids: Vec<i32>) -> Vec<ActiveModel> {
-    let mut seed_users: Vec<ActiveModel> = vec![];
+use fake::faker::lorem::en::Paragraph;
+use fake::Fake;
+use sea_orm::{ActiveModelTrait, DbErr, Set, TransactionTrait};
+use server::entities::post::{self, ActiveModel};
+use server::entities::user::ActiveModel as UserActiveModel;
+use server::misc::get_db_connection;
 
-    while mock_users.len() != num {
-        mock_users.push(user::ActiveModel {
-            first_name: Set(FirstName(EN).fake()),
-            last_name: Set(LastName(EN).fake()),
-            email: Set(SafeEmail(EN).fake()),
-            ..Default::default()
-        })
+/**
+ * This function will accept a `num` and a vector of user ids.
+ * It will create a `num` amount of posts for each user.
+ */
+pub fn generate_posts_seed(num: usize, users: Vec<UserActiveModel>) -> Vec<ActiveModel> {
+    let mut posts_seed: Vec<ActiveModel> = vec![];
+    let mut post_id: i32 = 0;
+
+    for user in users {
+        let mut i = 0;
+        while i != num {
+            posts_seed.push(post::ActiveModel {
+                content: Set(Paragraph(1..2).fake()),
+                user_id: Set(user.id.clone().unwrap()),
+                id: Set((post_id + 1) as i32),
+                ..Default::default()
+            });
+            i += 1;
+            post_id += 1;
+        }
     }
 
-    mock_users
+    posts_seed
 }
 
-async fn seed_posts() {}
+pub async fn seed_posts(posts: Vec<ActiveModel>) -> Result<(), DbErr> {
+    // Get the connection and start a transaction
+    let db = get_db_connection().await?;
+    let transaction = db.begin().await?;
+
+    // Insert with the transaction connection
+    for post in posts {
+        post.insert(&transaction).await?;
+    }
+
+    // Commit it
+    transaction.commit().await?;
+
+    Ok(())
+}
