dd8a8179702cf89e369edc5ac0096da9438f10df
zarif-al <zarif_al96@outlook.com>
2024-01-05 12:12:18 +0600

    feat: define user update function

---
+++ server/src/repositories/user/update.rs
@@ -0,0 +1,54 @@
+use async_graphql::{Error, InputObject, Result};
+use sea_orm::{ActiveModelTrait, DatabaseConnection, EntityTrait, Set};
+
+use crate::entities::user::{self, ActiveModel};
+
+use super::GraphQLUser;
+
+#[derive(InputObject)]
+pub struct UpdateUserInput {
+    id: i32,
+    first_name: Option<String>,
+    last_name: Option<String>,
+}
+
+pub async fn update_user(db: &DatabaseConnection, input: UpdateUserInput) -> Result<GraphQLUser> {
+    // Validate inputs
+    if input.first_name.is_none() && input.last_name.is_none() {
+        return Err(Error::new("I100"));
+    }
+
+    // Get user model
+    let result = user::Entity::find_by_id(input.id).one(db).await;
+
+    match result {
+        Ok(user_option) => match user_option {
+            Some(user) => {
+                let mut user: ActiveModel = user.into();
+
+                if let Some(first_name) = input.first_name {
+                    user.first_name = Set(first_name);
+                }
+
+                if let Some(last_name) = input.last_name {
+                    user.last_name = Set(last_name);
+                }
+
+                let update_result = user.update(db).await;
+
+                match update_result {
+                    Ok(user) => Ok(user.into()),
+                    Err(e) => {
+                        tracing::error!("Source: Update user. Message: {}", e.to_string());
+                        Err(Error::new("500"))
+                    }
+                }
+            }
+            None => Err(Error::new("U100")),
+        },
+        Err(e) => {
+            tracing::error!("Source: Update user. Message: {}", e.to_string());
+            Err(Error::new("500"))
+        }
+    }
+}
