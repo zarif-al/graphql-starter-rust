b72cbcd18b415c4b8c266d0342ced447040591c3
Abdullah Al Zarif <zarif_al96@outlook.com>
2024-06-19 15:50:12 +0600

    refactor: update type for id and user_id

--- server/src/repositories/post/mod.rs
+++ server/src/repositories/post/mod.rs
@@ -4,21 +4,21 @@ pub mod find_many;
 pub mod find_one;
 pub mod update;
 
-use crate::entities;
+use crate::entities::post::Model as PostModel;
 use async_graphql::SimpleObject;
 
 #[derive(SimpleObject)]
 #[cfg_attr(test, derive(PartialEq, Debug))]
 pub struct GraphQLPost {
-    pub id: i32,
+    pub id: String,
     pub created_at: i64,
     pub updated_at: i64,
     pub content: String,
-    pub user_id: i32,
+    pub user_id: String,
 }
 
-impl From<entities::post::Model> for GraphQLPost {
-    fn from(value: entities::post::Model) -> Self {
+impl From<PostModel> for GraphQLPost {
+    fn from(value: PostModel) -> Self {
         GraphQLPost {
             created_at: value.created_at.timestamp(),
             updated_at: value.updated_at.timestamp(),
