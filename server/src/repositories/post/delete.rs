7db1021269432a95b147d8bb6106f781dee1c147
Abdullah Al Zarif <zarif_al96@outlook.com>
2024-06-19 11:47:05 +0600

    refactor: use `user_email` instead of `id` to delete post

--- server/src/repositories/post/delete.rs
+++ server/src/repositories/post/delete.rs
@@ -1,43 +1,38 @@
-use crate::entities::prelude::Post;
+use crate::entities::prelude::{Post, User};
 use async_graphql::{Error, InputObject, Result};
 use sea_orm::{DatabaseConnection, EntityTrait, ModelTrait};
+use tracing::error;
 
 #[derive(InputObject)]
 pub struct DeletePostInput {
-    id: i32,
-    user_id: i32,
+    id: String,
+    user_email: String,
 }
 
 pub async fn delete_post(db: &DatabaseConnection, input: DeletePostInput) -> Result<bool> {
-    let result = Post::find_by_id(input.id).one(db).await;
+    let result = Post::find_by_id(input.id)
+        .find_also_related(User)
+        .one(db)
+        .await;
 
-    match result {
-        Ok(post_option) => match post_option {
-            Some(post) => {
-                // Check if post belongs to user
-                if post.user_id == input.user_id {
-                    let result = post.delete(db).await;
+    // Extract post and user information from the result
+    if let Ok(Some((post, optional_user))) = result {
+        // Proceed with deletion logic if user is not NONE
+        if let Some(user) = optional_user {
+            if user.email == input.user_email {
+                let result = post.delete(db).await;
 
-                    match result {
-                        Ok(_) => Ok(true),
-                        Err(e) => {
-                            tracing::error!("Source: Delete post. Message: {}", e.to_string());
-                            Err(Error::new("500"))
-                        }
+                match result {
+                    Ok(_) => return Ok(true),
+                    Err(e) => {
+                        error!("Post -> Delete: {}", e.to_string());
+                        return Err(Error::new("500"));
                     }
-                } else {
-                    // Error: Post does not belong to user
-                    Err(Error::new("P101"))
                 }
             }
-            None => {
-                // Error: Post not found
-                Err(Error::new("P100"))
-            }
-        },
-        Err(e) => {
-            tracing::error!("Source: Delete post. Message: {}", e.to_string());
-            Err(Error::new("500"))
         }
     }
+
+    // Error: Post not found
+    Err(Error::new("P100"))
 }
