d0133dfe5b7073d7b960a141d0e47c8eb49625b0
Abdullah Al Zarif <zarif_al96@outlook.com>
2024-06-19 17:24:18 +0600

    refactor: cleanup nested match

--- server/src/repositories/user/update.rs
+++ server/src/repositories/user/update.rs
@@ -1,5 +1,6 @@
 use async_graphql::{Error, InputObject, Result};
 use sea_orm::{ActiveModelTrait, DatabaseConnection, EntityTrait, Set};
+use tracing::error;
 
 use crate::entities::{prelude::User, user::ActiveModel};
 
@@ -7,48 +8,51 @@ use super::GraphQLUser;
 
 #[derive(InputObject)]
 pub struct UpdateUserInput {
-    id: i32,
+    id: String,
     first_name: Option<String>,
     last_name: Option<String>,
+    email: Option<String>,
 }
 
 pub async fn update_user(db: &DatabaseConnection, input: UpdateUserInput) -> Result<GraphQLUser> {
+    // TODO: Check if inputs can be SOME and EMPTY
     // Validate inputs
-    if input.first_name.is_none() && input.last_name.is_none() {
+    if input.first_name.is_none() && input.last_name.is_none() && input.email.is_none() {
         return Err(Error::new("I100"));
     }
 
     // Get user model
     let result = User::find_by_id(input.id).one(db).await;
 
-    match result {
-        Ok(user_option) => match user_option {
-            Some(user) => {
-                let mut user: ActiveModel = user.into();
+    // If we get an error log it and return 500
+    if let Err(err) = result {
+        error!("User -> Update: {}", err.to_string());
+        return Err(Error::new("500"));
+    }
 
-                if let Some(first_name) = input.first_name {
-                    user.first_name = Set(first_name);
-                }
+    if let Ok(Some(user)) = result {
+        let mut user: ActiveModel = user.into();
 
-                if let Some(last_name) = input.last_name {
-                    user.last_name = Set(last_name);
-                }
+        if let Some(first_name) = input.first_name {
+            user.first_name = Set(first_name);
+        }
 
-                let update_result = user.update(db).await;
+        if let Some(last_name) = input.last_name {
+            user.last_name = Set(last_name);
+        }
 
-                match update_result {
-                    Ok(user) => Ok(user.into()),
-                    Err(e) => {
-                        tracing::error!("Source: Update user. Message: {}", e.to_string());
-                        Err(Error::new("500"))
-                    }
-                }
+        let update_result = user.update(db).await;
+
+        match update_result {
+            Ok(user) => return Ok(user.into()),
+            Err(e) => {
+                error!("User -> Update: {}", e.to_string());
+                return Err(Error::new("500"));
             }
-            None => Err(Error::new("U100")),
-        },
-        Err(e) => {
-            tracing::error!("Source: Update user. Message: {}", e.to_string());
-            Err(Error::new("500"))
         }
     }
+
+    // If the Err() and Ok() block did not get triggered
+    // then we can assume the user was not found.
+    return Err(Error::new("U100"));
 }
