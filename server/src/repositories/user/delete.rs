0886b7b4dceba1cdf4c09ea0ce75df41a534da89
Abdullah Al Zarif <zarif_al96@outlook.com>
2024-06-19 16:54:19 +0600

    refactor: cleanup delete function

--- server/src/repositories/user/delete.rs
+++ server/src/repositories/user/delete.rs
@@ -1,36 +1,40 @@
 use async_graphql::{Error, InputObject, Result};
-use sea_orm::{DatabaseConnection, EntityTrait, ModelTrait};
+use sea_orm::{error, DatabaseConnection, EntityTrait, ModelTrait};
+use tracing::error;
 
-use server::entities::prelude::User;
+use crate::entities::prelude::User;
 
 #[derive(InputObject)]
 pub struct DeleteUserInput {
-    id: i32,
+    id: String,
 }
 
 pub async fn delete_user(db: &DatabaseConnection, input: DeleteUserInput) -> Result<bool> {
     // Find the user
     let result = User::find_by_id(input.id).one(db).await;
 
-    match result {
-        Ok(user_option) => match user_option {
-            // If user exists proceed to user deletion
-            Some(user) => {
-                let result = user.delete(db).await;
+    // If we get an error log it and return 500
+    if let Err(err) = result {
+        error!("Post -> Delete: {}", err.to_string());
+        return Err(Error::new("500"));
+    }
 
-                match result {
-                    Ok(_) => Ok(true),
-                    Err(e) => {
-                        tracing::error!("Source: Delete Error. Message: {}", e.to_string());
-                        Err(Error::new("500"))
-                    }
-                }
+    // Extract the user and proceed to deletion
+    if let Ok(Some(user)) = result {
+        let result = user.delete(db).await;
+
+        match result {
+            Ok(_) => {
+                return Ok(true);
             }
-            None => Err(Error::new("U100")),
-        },
-        Err(e) => {
-            tracing::error!("Source: Delete Error. Message: {}", e.to_string());
-            Err(Error::new("500"))
+            Err(e) => {
+                error!("Post -> Delete: {}", e.to_string());
+                return Err(Error::new("500"));
+            }
         }
     }
+
+    // If the Ok() and Err() block did not get triggered then we
+    // can assume the user was not found
+    return Err(Error::new("U100"));
 }
