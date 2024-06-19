1cfa468cfc91f2ba52d10b7a2a6446f430d26bee
Abdullah Al Zarif <zarif_al96@outlook.com>
2024-06-19 12:14:12 +0600

    feat: use `user_email` instead of `user_id`

--- server/src/repositories/post/create.rs
+++ server/src/repositories/post/create.rs
@@ -1,12 +1,13 @@
-use crate::entities::{post, prelude::User};
+use crate::entities::prelude::User;
 use async_graphql::{Error, InputObject, Result};
-use sea_orm::{ActiveModelTrait, DatabaseConnection, EntityTrait, Set};
+use sea_orm::{ActiveModelTrait, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter, Set};
+use server::entities::{post, user};
 
 use super::GraphQLPost;
 
 #[derive(InputObject)]
 pub struct CreatePostInput {
-    user_id: i32,
+    user_email: String,
     content: String,
 }
 
@@ -17,35 +18,32 @@ pub async fn create_post(db: &DatabaseConnection, input: CreatePostInput) -> Res
     }
 
     // Search for user with provided user_id.
-    let user_search_result = User::find_by_id(input.user_id).one(db).await;
+    let user_result = User::find()
+        .filter(user::Column::Email.eq(input.user_email))
+        .one(db)
+        .await;
 
-    match user_search_result {
-        Ok(user_option) => {
-            // If user exists create post otherwise throw error.
-            if user_option.is_some() {
-                let new_post = post::ActiveModel {
-                    content: Set(input.content),
-                    user_id: Set(input.user_id),
-                    ..Default::default()
-                };
+    // Proceed with post creation logic if user is found
+    if let Ok(Some(user)) = user_result {
+        let new_post = post::ActiveModel {
+            content: Set(input.content),
+            user_id: Set(user.id),
+            ..Default::default()
+        };
 
-                let result = new_post.insert(db).await;
+        let result = new_post.insert(db).await;
 
-                match result {
-                    Ok(post_model) => Ok(post_model.into()),
-                    Err(e) => {
-                        tracing::error!("Source: Create Post. Message: {}", e.to_string());
-                        Err(Error::new("500"))
-                    }
-                }
-            } else {
-                // Error: User does not exist
-                return Err(Error::new("U100"));
+        match result {
+            Ok(post_model) => {
+                return Ok(post_model.into());
+            }
+            Err(e) => {
+                tracing::error!("Source: Create Post. Message: {}", e.to_string());
+                return Err(Error::new("500"));
             }
         }
-        Err(e) => {
-            tracing::error!("Source: Create Post. Message: {}", e.to_string());
-            Err(Error::new("500"))
-        }
     }
+
+    // Error: User does not exist
+    return Err(Error::new("U100"));
 }
