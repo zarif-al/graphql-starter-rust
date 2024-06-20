3eb2392c0dc332f2777c1260b3ffcca12ddbebca
Abdullah Al Zarif <zarif_al96@outlook.com>
2024-06-20 12:45:00 +0600

    refactor: remove `misc` module

--- server/src/misc/mod.rs
+++
@@ -1,8 +0,0 @@
-/// This module will contain miscellaneous code that is necessary throughout the project
-///
-/// If a sub module of this module only has one function then we can simply re-export it.
-pub mod connection;
-pub mod env;
-pub mod responses;
-
-pub use connection::get_db_connection;
